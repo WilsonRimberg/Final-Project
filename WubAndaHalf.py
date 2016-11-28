32ba5ffa2f6b0016746fa1c5b4ed9c385aef227d
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-11-28 15:39:53 -0500

    Updated from Brython Server: 11/28/2016 3:39:52 PM

--- WubAndaHalf.py
+++ WubAndaHalf.py
@@ -3,7 +3,11 @@ from ggame import LineStyle, Color, Sprite, Sound, Frame
 
 SCREEN_WIDTH = 1420
 SCREEN_HEIGHT = 810
-
+potato=800
+background_asset1=ImageAsset("images/Green.png",)
+background_asset2=ImageAsset("images/starfield.jpg",)
+background1=Sprite(background_asset1, (0,0))
+background2=Sprite(background_asset2, (0,0))
 spaceship_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
 spaceship = Sprite(spaceship_asset, (740, 405))
@@ -27,14 +31,14 @@ def down(b):
 def step():
     if spaceship.go:
         spaceship.x += spaceship.dir
+        if spaceship.x + spaceship.width > potato:
+            background1=Sprite(background_asset2, (0,0))
         if spaceship.x + spaceship.width > SCREEN_WIDTH:
             spaceship.x -= spaceship.dir
             spaceship.rotation=(3.141592653589793238462643383/2)
-            reverse(spaceship)
         if spaceship.x < 60:
             spaceship.x -= spaceship.dir
             spaceship.rotation=((3*3.141592653589793238462643383)/2)
-            reverse(spaceship)
         if spaceship.thrust == 1:
             spaceship.setImage(spaceship.thrustframe)
             spaceship.thrustframe += 1
@@ -50,11 +54,9 @@ def ystep():
         if spaceship.y +spaceship.height > SCREEN_HEIGHT+60:
             spaceship.y -= spaceship.bob
             spaceship.rotation=0
-            reverse(spaceship)
         if spaceship.y < 60:
             spaceship.y -= spaceship.bob
             spaceship.rotation=3.141592653589793238462643383
-            reverse(spaceship)
         if spaceship.thrust == 1:
             spaceship.setImage(spaceship.thrustframe)
             spaceship.thrustframe += 1
